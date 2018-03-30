851387b1adeddc1d35e1d6e8667397bc759ca24d
Jorge Atempa <atempaitm@gmail.com>
2018-03-29 21:43:11 -0700

    Create a 'Movie' entity

---
+++ app/src/main/java/com/atempa/dam18_recyclerview/Movie.java
@@ -0,0 +1,49 @@
+package com.atempa.dam18_recyclerview;
+
+public class Movie {
+    private String title;
+    private String description;
+    private String director;
+    private String poster;
+    private String relase;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getRelase() {
+        return relase;
+    }
+
+    public void setRelase(String relase) {
+        this.relase = relase;
+    }
+}
